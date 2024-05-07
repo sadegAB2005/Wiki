0033573fc090597f5983c183a43caaef8be6f58f
sadeg <sadegaboein@proton.me>
2024-05-08 00:37:18 +0200

    submit

--- test.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        x = int(input('Enter value for x: '))
-        y = int(input('Enter value for y: '))
-        break
-    except ValueError:
-        print('Invalid input. Please enter numeric values for x and y.')
-
-# Rest of your code here...
